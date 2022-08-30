4f20ff109415824fed2cc6208a19ddbbb3f829bf
Dzenana95 <dzenana_zahirovic95@gmail.com>
2022-08-30 23:50:48 +0200

    amra

---
+++ ispisime.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+int main()
+{	
+	std::string ime;
+	std::cout<<"Unesite svoje ime: "<<std::endl;
+	std::cin>>ime;
+	std::cout<<"Pozdrav"  <<ime<< " !";
+	return 0;
+}
+	
