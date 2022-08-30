783645fab6709418e90c639f86e431f2ca0ea467
adoDr <adnan.sahanic@fet.ba>
2022-08-31 00:05:45 +0200

    Adnanova verzija

--- ispisime.cpp
+++ ispisime.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<string>
 
+using namespace std;
 int main()
-{	
+{	int g;
 	std::string ime;
 	std::cout<<"Unesite svoje ime: "<<std::endl;
 	std::cin>>ime;
-	std::cout<<"Pozdrav"  <<ime<< " !";
+	std::cout<<"Pozdrav"  <<ime<< " !"<<endl;
+	cout<<"Unesite vase godine "<<endl;
+	cin>>g;
+	cout<<"Vase godine su:"<<g<<endl;
 	return 0;
 }
 	
